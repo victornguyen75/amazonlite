e34deaf7d013aa083322112aefb27d4724d655df
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2022-09-02 13:52:07 -0500

    feat(index.ts, Layout.tsx, index.tsx): add Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,43 @@
+import React, { Fragment } from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+interface LayoutProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export const Layout: React.FC<LayoutProps> = ({ title, children }) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>{title ? title + "- amazonlite" : "amazonlite"} </title>
+        <meta name="description" content="E-Commerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">amazonlite</a>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <a className="p-2">Cart</a>
+              </Link>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 amazonlite</p>
+        </footer>
+      </div>
+    </Fragment>
+  );
+};
+
+Layout.displayName = "Layout";
