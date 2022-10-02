c9ccff6c0637f37d79d2cf5a33d0a08396d5cfca
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2022-10-02 13:55:05 -0500

    feat(db.ts): add db connection

---
+++ utils/db.ts
@@ -0,0 +1,45 @@
+import mongoose, { ConnectionStates } from "mongoose";
+
+interface IConnection {
+  isConnected: ConnectionStates;
+}
+
+const connection: IConnection = {
+  isConnected: 0,
+};
+
+const connect = async () => {
+  if (connection.isConnected) {
+    console.log("Already connected");
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log("Use previous connection");
+      return;
+    }
+
+    await mongoose.disconnect();
+  }
+
+  const db = await mongoose.connect(process.env.MONGODB_URI);
+  console.log("new connection");
+  connection.isConnected = db.connections[0].readyState;
+};
+
+const disconnect = async () => {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === "production") {
+      await mongoose.disconnect();
+      connection.isConnected = 0;
+    } else {
+      console.log("Not disconnected");
+    }
+  }
+};
+
+const db = { connect, disconnect };
+
+export default db;
