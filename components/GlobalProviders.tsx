40ae6585defc3997a7112793e871b15c0b0007f1
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2022-10-20 19:24:11 -0500

    chore(GlobalProviders.tsx): add interface and types

--- components/GlobalProviders.tsx
+++ components/GlobalProviders.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
 import { ToastProvider } from "../components";
 import { StoreProvider } from "../utils";
 
-export const GlobalProviders = ({ session, children }): JSX.Element => (
+interface ProviderProps {
+  session: Session;
+  children: ReactNode;
+}
+
+declare type ISODateString = string;
+
+interface Session {
+  user?: {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
+  expires: ISODateString;
+}
+
+export const GlobalProviders = ({
+  session,
+  children,
+}: ProviderProps): JSX.Element => (
   <SessionProvider session={session}>
     <StoreProvider>
       <ToastProvider>{children}</ToastProvider>
