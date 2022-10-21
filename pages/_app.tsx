c0d5d5a6a35fc09e9264a82199cce3d2935476da
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2022-10-20 19:30:13 -0500

    chore(_app.tsx): add AppProps type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { AppProps } from "next/app";
 import { GlobalProviders } from "../components";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }): JSX.Element => {
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   const { session, ...props } = pageProps;
   return (
     <GlobalProviders session={session}>
