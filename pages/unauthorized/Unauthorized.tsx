bda6af021f2bbb302ab5e7513696092c411338f0
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2022-10-23 00:11:15 -0500

    feat(index.ts, Unauthorized.tsx): add unauthorized pages

---
+++ pages/unauthorized/Unauthorized.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Layout } from "../../components";
+
+export default function Unauthorized() {
+  const router = useRouter();
+  const { message } = router.query;
+
+  return (
+    <Layout title="Unauthorized Page">
+      <h1 className="text-xl">Access Denied</h1>
+      {message && <div className="mb-4 text-red-500">{message}</div>}
+    </Layout>
+  );
+}
+
+Unauthorized.displayName = "Unauthorized";
