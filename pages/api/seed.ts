cd89f775bc71936eb8be098a1ed903aaad17b806
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2022-10-25 19:17:12 -0500

    feat(Product.ts, seed.ts): upload products to mongo db via mongoose schema

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,3 +1,4 @@
+import Product from "models/Product";
 import User from "models/User";
 import { db, data } from "utils";
 
@@ -5,6 +6,8 @@ const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users); // can't figure out the type issue here
+  await Product.deleteMany();
+  await Product.insertMany(data.products); // can't figure out the type issue here
   await db.disconnect();
   res.send({ message: "seeded successfully" });
 };
