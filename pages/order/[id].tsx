8d618e0ecaf549a4edf5de3c8d2adf01254d419b
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2022-10-30 19:33:32 -0500

    style([id].tsx): fix styles and clean up code

--- pages/order/[id].tsx
+++ pages/order/[id].tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Layout } from "components";
-import { getError } from "utils";
+import { getError, Product } from "utils";
 
 export default function Order() {
   const {
@@ -48,107 +48,119 @@ export default function Order() {
   } = order;
 
   if (loading) {
-    return <div>Loading</div>;
+    return (
+      <Layout title={`Order ${orderId}`}>
+        <h1 className="mb-4 text-xl">{`Order ${orderId}`}</h1>
+        <div>Loading</div>
+      </Layout>
+    );
   } else if (error) {
-    return <div className="alert-error"> {error}</div>;
+    return (
+      <Layout title={`Order ${orderId}`}>
+        <h1 className="mb-4 text-xl">{`Order ${orderId}`}</h1>
+        <div className="alert-error">{error}</div>
+      </Layout>
+    );
   }
 
   return (
     <Layout title={`Order ${orderId}`}>
       <h1 className="mb-4 text-xl">{`Order ${orderId}`}</h1>
       <div className="grid md:grid-cols-4 md:gap-5">
-        <div className="card p-5">
-          <h2 className="mb-2 text-lg">Shipping Address</h2>
-          <div>
-            {shippingAddress.fullName}, {shippingAddress.address},{" "}
-            {shippingAddress.city}, {shippingAddress.postalCode},{" "}
-            {shippingAddress.country}
+        <div className="overflow-x-auto md:col-span-3">
+          <div className="card p-5">
+            <h2 className="mb-2 text-lg">Shipping Address</h2>
+            <div>
+              {shippingAddress.fullName}, {shippingAddress.address},{" "}
+              {shippingAddress.city}, {shippingAddress.postalCode},{" "}
+              {shippingAddress.country}
+            </div>
+            {isDelivered ? (
+              <div className="alert-success">Delivered at {deliveredAt}</div>
+            ) : (
+              <div className="alert-error">Not delivered</div>
+            )}
           </div>
-          {isDelivered ? (
-            <div className="alert-success">Delivered at {deliveredAt}</div>
-          ) : (
-            <div className="alert-error">Not delivered</div>
-          )}
-        </div>
 
-        <div className="card p-5">
-          <h2 className="mb-2 text-lg">Payment Method</h2>
-          <div>{paymentMethod}</div>
-          {isPaid ? (
-            <div className="alert-success">Paid at {paidAt}</div>
-          ) : (
-            <div className="alert-error">Not Paid</div>
-          )}
-        </div>
+          <div className="card p-5">
+            <h2 className="mb-2 text-lg">Payment Method</h2>
+            <div>{paymentMethod}</div>
+            {isPaid ? (
+              <div className="alert-success">Paid at {paidAt}</div>
+            ) : (
+              <div className="alert-error">Not Paid</div>
+            )}
+          </div>
 
-        <div className="card overflow-x-auto p-5">
-          <h2 className="mb-2 text-lg">Order Items</h2>
-          <table className="min-w-full">
-            <thead className="border-b">
-              <tr>
-                <th className="px-5 text-left">Item</th>
-                <th className="    p-5 text-right">Quantity</th>
-                <th className="  p-5 text-right">Price</th>
-                <th className="p-5 text-right">Subtotal</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orderItems.map((item) => (
-                <tr key={item._id} className="border-b">
-                  <td>
-                    <Link href={`/product/${item.slug}`}>
-                      <a className="flex items-center">
-                        <Image
-                          src={item.image}
-                          alt={item.name}
-                          width={50}
-                          height={50}
-                        ></Image>
-                        &nbsp;
-                        {item.name}
-                      </a>
-                    </Link>
-                  </td>
-                  <td className=" p-5 text-right">{item.cartCount}</td>
-                  <td className="p-5 text-right">${item.price}</td>
-                  <td className="p-5 text-right">
-                    ${item.cartCount * item.price}
-                  </td>
+          <div className="card overflow-x-auto p-5">
+            <h2 className="mb-2 text-lg">Order Items</h2>
+            <table className="min-w-full">
+              <thead className="border-b">
+                <tr>
+                  <th className="px-5 text-left">Item</th>
+                  <th className="    p-5 text-right">Quantity</th>
+                  <th className="  p-5 text-right">Price</th>
+                  <th className="p-5 text-right">Subtotal</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {orderItems.map((item: Product) => (
+                  <tr key={item._id} className="border-b">
+                    <td>
+                      <Link href={`/product/${item.slug}`}>
+                        <a className="flex items-center">
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          ></Image>
+                          &nbsp;
+                          {item.name}
+                        </a>
+                      </Link>
+                    </td>
+                    <td className=" p-5 text-right">{item.cartCount}</td>
+                    <td className="p-5 text-right">${item.price}</td>
+                    <td className="p-5 text-right">
+                      ${item.cartCount * item.price}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-      <div>
-        <div className="card  p-5">
-          <h2 className="mb-2 text-lg">Order Summary</h2>
-          <ul>
-            <li>
-              <div className="mb-2 flex justify-between">
-                <div>Items</div>
-                <div>${itemsPrice}</div>
-              </div>
-            </li>{" "}
-            <li>
-              <div className="mb-2 flex justify-between">
-                <div>Tax</div>
-                <div>${taxPrice}</div>
-              </div>
-            </li>
-            <li>
-              <div className="mb-2 flex justify-between">
-                <div>Shipping</div>
-                <div>${shippingPrice}</div>
-              </div>
-            </li>
-            <li>
-              <div className="mb-2 flex justify-between">
-                <div>Total</div>
-                <div>${totalPrice}</div>
-              </div>
-            </li>
-          </ul>
+        <div>
+          <div className="card  p-5">
+            <h2 className="mb-2 text-lg">Order Summary</h2>
+            <ul>
+              <li>
+                <div className="mb-2 flex justify-between">
+                  <div>Items</div>
+                  <div>${itemsPrice}</div>
+                </div>
+              </li>{" "}
+              <li>
+                <div className="mb-2 flex justify-between">
+                  <div>Tax</div>
+                  <div>${taxPrice}</div>
+                </div>
+              </li>
+              <li>
+                <div className="mb-2 flex justify-between">
+                  <div>Shipping</div>
+                  <div>${shippingPrice}</div>
+                </div>
+              </li>
+              <li>
+                <div className="mb-2 flex justify-between">
+                  <div>Total</div>
+                  <div>${totalPrice}</div>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </Layout>
