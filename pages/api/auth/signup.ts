b8452811bd645e3b69ed5caf16ececfea26c5c9a
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2022-11-12 11:34:31 -0600

    feat(api/auth/signup.ts): create new accounts

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,47 @@
+import bcryptjs from "bcryptjs";
+import User from "models/User";
+import { db } from "utils";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+
+  const { name, email, password } = req.body;
+  if (
+    !name ||
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 5
+  ) {
+    res.status(422).json({ message: "Validation error." });
+    return;
+  }
+
+  await db.connect();
+
+  const existingUser = await User.findOne({ email: email });
+  if (existingUser) {
+    res.status(422).json({ message: "User already exists." });
+    await db.disconnect();
+    return;
+  }
+
+  const newUser = new User({
+    name,
+    email,
+    password: bcryptjs.hashSync(password),
+    isAdmin: false,
+  });
+
+  const user = await newUser.save();
+  await db.disconnect();
+  res.status(201).send({
+    message: "User created!",
+    _id: user.id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  });
+}
